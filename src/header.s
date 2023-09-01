c6b5442b3fd5283ef133213d78723ccc36675a6c
Persune <gumball2415@gmail.com>
2023-09-02 01:01:45 +0800

    need ines 2 for 32k chr ram

--- src/header.s
+++ src/header.s
@@ -1,19 +1,19 @@
 .import nmi_handler, reset_handler, irq_handler
 
-; iNES 1.0
+; iNES 2.0
 .segment "HEADER"
   ; comments regarding header format taken from
-  ; https://wiki.nesdev.org/w/index.php?title=INES#iNES_file_format
+  ; https://www.nesdev.org/wiki/index.php?title=INES#iNES_file_format
+  ; https://www.nesdev.org/wiki/NES_2.0#File_Structure
   ; Flag 0-3
   .byte "NES", $1A
   ; Flag 4
-  .byte $01                       ; n * 16KB PRG ROM
+  .byte $04                       ; n * 16KB PRG ROM
   ; Flag 5
-  .byte $01                       ; n * 8KB CHR ROM
+  .byte $00                       ; n * 8KB CHR ROM
 
   ; Flag 6
-  .byte %00000000
-  ;      ||||||||
+  .byte %11000000
   ;      |||||||+- Mirroring: 0: horizontal (vertical arrangement) (CIRAM A10 = PPU A11)
   ;      |||||||              1: vertical (horizontal arrangement) (CIRAM A10 = PPU A10)
   ;      ||||||+-- 1: Cartridge contains battery-backed PRG RAM ($6000-7FFF) or other persistent memory
@@ -22,24 +22,29 @@
   ;      ++++----- Lower nybble of mapper number
 
   ; Flag 7
-  .byte %00000000
-  ;      ||||||||
-  ;      |||||||+- VS Unisystem
-  ;      ||||||+-- PlayChoice-10 (8KB of Hint Screen data stored after CHR data)
-  ;      ||||++--- If equal to 2, flags 8-15 are in NES 2.0 format
-  ;      ++++----- Upper nybble of mapper number
+  .byte %00011000
+  ;      ||||||++-- Console type
+  ;      ||||||      0: Nintendo Entertainment System/Family Computer
+  ;      ||||||      1: Nintendo Vs. System
+  ;      ||||||      2: Nintendo Playchoice 10
+  ;      ||||||      3: Extended Console Type
+  ;      ||||++---- NES 2.0 identifier
+  ;      ++++------ Mapper Number D4..D7
 
-  ; Flag 8
-  .byte $00                      ; PRG-RAM size (rarely used extension)
+  ; Flag 8-10
+  .byte $00, $00, $00
 
-  ; Flag 9
-  .byte %00000000
-  ;      ||||||||
-  ;      |||||||+- TV system (0: NTSC; 1: PAL)
-  ;      +++++++-- Reserved, set to zero
+  ; Flag 11
+  .byte $09
+  ;      cC
+  ;      |+----- CHR-RAM size (volatile) shift count
+  ;      +------ CHR-NVRAM size (non-volatile) shift count
+  ;     If the shift count is zero, there is no CHR-(NV)RAM.
+  ;     If the shift count is non-zero, the actual size is
+  ;     "64 << shift count" bytes, i.e. 8192 bytes for a shift count of 7.
 
-  ; Flag 10-15; unused
-  .byte $00, $00, $00, $00, $00, $00
+  ; Flag 12-15; unused
+  .byte $00, $00, $00, $00
 
 .segment "VECTORS"
 .addr nmi_handler, reset_handler, irq_handler
